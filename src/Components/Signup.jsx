0d96964eb0eb3a707e06cc8e76447557ebc75052
Jashwa <jashwa46733@gmail.com>
2024-05-24 22:12:42 +0530

    Update Signup.jsx

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -7,6 +7,7 @@ const Signup = () => {
 
 const[email, setEmail] = useState('')
 const[password, setPassword] = useState('')
+const[alert , setAlert] = useState(true)
 // console.log(email , Password)
 
 const passing = useNavigate()
@@ -14,14 +15,16 @@ const passing = useNavigate()
     e.preventDefault()
     await axios.post("https://doll-delights-backend-project-2.onrender.com/signin",{email , password}).then((res)=>{
           if (res.data == "exist") {
-            alert("user id already exist")
+            // alert("user id already exist")
+            setAlert(false)
           } else {
             passing('/login')
+            setAlert(true)
           }
      })
   }
   return (
-    <section className='my-[50px]'>
+    <section className=' h-lvh flex items-center justify-center'>
 
       <div className="w-[93%] mx-auto poppins">
       
@@ -32,7 +35,12 @@ const passing = useNavigate()
         <div className='flex flex-col items-start gap-[30px] py-[15px]'>
             <input type="text" placeholder='Email address' className='lg:w-[450px] w-full border-[1px] border-black opacity-55 py-[15px] pl-3 rounded-[6px]' onChange={(e)=>setEmail(e.target.value)} required/>
             <input type="text"  placeholder='Password' className='lg:w-[450px] w-full border-[1px] border-black opacity-55 py-[15px] pl-3 rounded-[6px]' onChange={(e)=>setPassword(e.target.value)}  required/>
+           
            </div>
+
+         {
+          alert === false ?   <p className='text-red-700 text-center pb-2 font-semibold'>User ID already exist !</p> : null
+         }
            <p className='flex items-center lg:gap-[5px] gap-[3px] justify-center lg:text-[20px] text-[12px]'>Already have an Account ?<Link to={'/login'}><p className='text-btn-color font-normal lg:text-[20px] text-[12px] underline'>Login</p></Link></p>
            <button className='w-full py-[11px] bg-btn-color rounded-[5px] font-medium text-[19px] my-[20px]' type='submit'>Register</button>
         </form>
